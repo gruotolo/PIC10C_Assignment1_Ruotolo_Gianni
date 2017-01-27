afbf66d31b18da911beb5c17a75f6061641a40d7
Gianni Ruotolo <gru_10@hotmail.com>
2017-01-27 02:27:41 -0800

    added return total rank function and a while loop in main that asks user to add more cards
    when the user has the choice to add more cards the addcard function is
    called to add more cards and the total is updated and returned. then the
    question is asked again in the new while loop added.

--- PIC10C_Assignment1/siete_y_medio.cpp
+++ PIC10C_Assignment1/siete_y_medio.cpp
@@ -24,11 +24,25 @@ int main() {
 	//while (you.returnmoney() > 0 && dealer.returnmoney() > 0)
 	//{
 		int bet = 0;
+		char answer = 'y';
 		cout << "you have $" << you.returnmoney() << ".  Enter bet: ";
 		cin >> bet;
 		cout << "\nYour cards: ";
 		Hand yourhand;
-		cout << "your total is ";
+		cout << "\nyour total is :" << yourhand.returntotalrank();
+		cout << "    Do you want another card? (y/n):";
+		cin >> answer;
+
+		while (answer == 'y')
+		{
+			cout << "\n new card: \n";
+			yourhand.addcard();
+			cout << "\n your cards: \n";
+				//insert print cards here
+			cout << "\n your total is: " << yourhand.returntotalrank();
+			cout << "    Do you want another card? (y/n):";
+			cin >> answer;
+		}
 		cin.get();
 		cin.get();
 
